78b3c86e0aba52f3dc7dc6f31d7922f58eb0e95e
Alexander Tenbits <source.kit@gmail.com>
2013-01-03 16:26:27 +0100

    [+]template starter: autoreload, build.js template

--- template/starter/include.routes.js
+++ template/starter/include.routes.js
@@ -1,1 +1,9 @@
+window.DEBUG = true;
+
 include.routes(%ROUTES%);
+
+if (window.DEBUG && window.location.hash.indexOf('!watch') > -1){
+	include.embed({
+		lib: 'include/include.autoreload'
+	});
+}
