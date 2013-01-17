2cd99bdbf41309067cf79b3ffc47fcc8d440c546
Alexander Tenbits <source.kit@gmail.com>
2013-01-17 20:28:33 +0100

    [impr] open browser at http://localhost:PORT/OPEN after server has started

--- src/action/server.js
+++ src/action/server.js
@@ -1,10 +1,28 @@
 (function(){
     
+    /**
+     *  CONFIG
+     *
+     *      action: 'server',
+     *      open: 'url path to open in browser after server has started', is DEFAULT for next raw argument
+     *      port: PORT to start at localhost
+     */
+    
     include.exports = {
         process: function(config, idfr){
+            
+            
+            if (config.args){
+                config.open = config.args[0];
+            }
+            
             include.js('/src/server/server.js').done(function(resp){
                 
                 resp.server.start(config);
+                
+                if (config.open){
+                    require('openurl').open(String.format('http://localhost:%1/%2', config.port || 5777, config.open));
+                }
                 
                 idfr && idfr.resolve();
             });
