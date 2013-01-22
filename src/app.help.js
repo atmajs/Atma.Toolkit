bb6af01eac9ed6275c74aa7f9811ad6237e0a46d
Alexander Kit <source.kit@gmail.com>
2013-01-22 22:37:03 +0100

    [+] cli commands

--- src/app.help.js
+++ src/app.help.js
@@ -1,63 +1,58 @@
 
 
 
-var program = require('commander');
+var program = require('commander'),
+    currentIndent;
 
 
 function write(message, indent) {
+    if (indent != null){
+        currentIndent = indent;  
+    } 
     var _ = '';
-    if (indent) for (var i = 0; i < indent; i++) _ += '   ';
+    if (currentIndent) for (var i = 0; i < currentIndent; i++) _ += '   ';
     console.log(_ + message);
 }
 
 
-program.usage('[command] [options]') //
+program.usage('[action | *.config | *.js] [arg] [...] -KEY VALUE [...]') //
 .description('IncludeJS Application Builder') //
-.version('0.3.5') //
-.option('-m, --minify', 'Minify Javascript and CSS output')//
-.option ('-action XXX', 'Sets or Overrides config action property');
+.version('0.5.6') //
 
 
 program.on('--help', function() {
     var help = [];
 
-    write('[command] - ', 1);
+    write('Actions - ', 1);
     write('\r');
 
-    write('[*.hml] [options]', 2);
-    write('Parse HTML and build all resources', 3);
-    write('[options] @see program options', 4);
-    
-    write('');
-    write('[*.config]', 2);
-    write('{JSON}', 3);
-    write('');
-    
-    write('"file" - *.html input',3);
-    
-    //write('"vars" - {Object} - Variables used when parsing includes',3);
-    
-    write('"action" - ',3);
-    write('"build" - @default - combine resources',4);
-    write('"import" - copy outer resources to projects directory',4);
-	write('"reference" - switch back from "import" to resource referencing',4);
-    
+    write('build\t collect and combine all includes starting from main html', 2);
+    write('shell\t run shell command/-s');
+    write('custom\t run custom script/-s');
+    write('server\t start local server from current woring dir');
+    write('git-clone\t clone all LibJS libraries');
+    write('globals\t open includejs.builder routes file');
+    write('template\t create template project in current dir');
+    write('reference\t create symbolic link to a folder in current dir');
+
+    write('\r',1);
+
+    write('Options - ', 1);
+
+    write('build - ', 2);
     write('"minify" - {Boolean} - available only with action == "build"',3);
     write('"outputMain" - output name of a built html',3);
     write('"outputSources" - directory of built resources',3);
-	
-	write('"uglify" - {Object} - UglifyJS settings. Example, {global_defs:{DEBUG:false}}',3);
+    write('"uglify" - {Object} - UglifyJS settings. Example, {global_defs:{DEBUG:false}}',3);
     write('"jshint" - {Object} - Hint settings',3);
     write('"options" - {Object} - JSHINT options',4);
     write('"global" - {Object} - JSHINT global variables',4);
     write('"ignore" - {Object} - Filenames hash to ignore',4);
-	
-    
-    
-    write('');
-    write('template [name]',2)
-    write('Create empty project in {current} directory',3);
-    write('Available: "starter"',4);
+
+    write('\r',1);
+
+
+    write('Further options at http://libjs.it/#/includeBuilder/commands')
     
 });
 
