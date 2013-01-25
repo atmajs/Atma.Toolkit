03c5fedff25fe6674b6a3ce32319a9657bc29121
Alexander Kit <source.kit@gmail.com>
2013-01-25 15:09:44 +0100

    [+] exposes color function to global

--- src/helper/stdout.js
+++ src/helper/stdout.js
@@ -1,34 +1,36 @@
 
-!function(){
-    
+(function(){
+
     include.exports = {
         color: (function(){
-            
+
             return function(str){
-                
+
                 function format(value){
                     return '\u001b['+value+'m$1\u001b[0m'
                 }
-                
+
                 var colors = {
                     red: format(31),
-                    green: format(32),                    
+                    green: format(32),
                     yellow: format(33),
                     blue: format(34),
-                    
+
                     bold: format(1),
                     italic: format(3),
                     underline: format(4),
                     inverse: format(7)
                 }
-                
+
                 for(var key in colors){
                     str = str.replace(new RegExp(key + '\\{([^\\}]+)\\}','g'), colors[key]);
                 }
-                
+
                 return str;
             }
         })()
-    }
-    
-}();
+    };
+
+    global.color = include.exports.color;
+
+}());
