766faa912632de960ac7d45db8ee8e7046e51a20
Alexander Kit <source.kit@gmail.com>
2013-01-28 12:35:40 +0100

    [+] add global definition

--- src/parser/util/includeMock.js
+++ src/parser/util/includeMock.js
@@ -1,33 +1,35 @@
-var Routes = global.includeLib.Routes();
+(function(global){
+	var Routes = global.includeLib.Routes();
 
-include.exports = Class({
-	Construct: function() {
-		this.includes = [];
-		ruqq.arr.each(['js', 'css', 'load', 'lazy'], function(x) {
-			this[x] = function(pckg) {
+	include.exports = Class({
+		Construct: function() {
+			this.includes = [];
+			ruqq.arr.each(['js', 'css', 'load', 'lazy'], function(x) {
+				this[x] = function(pckg) {
 
-				Routes.each(x, pckg, function(namespace, route) {
-					this.includes.push({
-						type: x,
-						url: route.path,
-						route: route,
-						namespace: namespace
-					});
-				}.bind(this));
-				return this;
+					Routes.each(x, pckg, function(namespace, route) {
+						this.includes.push({
+							type: x,
+							url: route.path,
+							route: route,
+							namespace: namespace
+						});
+					}.bind(this));
+					return this;
+				}
+			}.bind(this));
+		},
+		cfg: function() {
+			return this;
+		},
+		routes: function(arg){
+			if (arg == null){
+				return Routes.getRoutes();
 			}
-		}.bind(this));
-	},
-	cfg: function() {
-		return this;
-	},
-	routes: function(arg){
-		if (arg == null){
-			return Routes.getRoutes();
+			for (var key in arg) {
+				Routes.register(key, arg[key]);
+			}
+			return this;
 		}
-		for (var key in arg) {
-			Routes.register(key, arg[key]);
-		}
-		return this;
-	}
-});
+	});
+}(typeof window === 'undefined' ? global : window));
