c21d4de75ee2921e6785c4b425eca3ca20e8139b
Alexander Kit <source.kit@gmail.com>
2013-02-26 15:05:46 +0100

    [+] add .js if provided name has no extension and file not exists

--- src/action/custom.js
+++ src/action/custom.js
@@ -1,24 +1,34 @@
 include.exports = {
-    process: function(config, done){
+    process: function(config, done) {
 
         var script = config.script;
 
-        if (config.args && !script){
+        if (config.args && !script) {
             script = config.args[0];
         }
 
-        if (!(script && new io.File(script).exists())){
-            done && done(new Error(String.format('Custom script not exists: %1', script)));
+        if (!script) {
+            done('Custom script not defined - via cli: $ includejs custom name.js, via config: define script property');
+            return;
+        }
+
+        if (new io.File(script).exists() === false) {
+            script += '.js';
+        }
+
+        if (new io.File(script).exists() === false) {
+            done(String.format('Custom script not exists: %1', script));
+            return;
         }
 
         include.routes({
-            app: net.URI.combine('file:///',process.cwd(),'{0}')
+            app: net.URI.combine('file:///', process.cwd(), '{0}')
         });
 
 
         var _path = /[^;]+[\\\/]npm[\\\/][^;]*/g.exec(process.env.path);
 
-        globalPath = _path && _path[0].replace(/\\/g,'/');
+        globalPath = _path && _path[0].replace(/\\/g, '/');
         globalPath = net.URI.combine(globalPath, 'node_modules');
 
 
@@ -29,23 +39,23 @@ include.exports = {
 
         console.log(nodeModulesPath);
 
-        if (globalPath){
+        if (globalPath) {
             paths.push(globalPath);
         }
 
         include.js({
             app: script + '::Script'
-        }).done(function(resp){
+        }).done(function(resp) {
 
-            if (resp.Script && resp.Script.process){
+            if (resp.Script && resp.Script.process) {
                 resp.Script.process(config, done);
-            }else{
+            } else {
                 console.error("Custom Script should exports 'process' function: include.exports = { process: function(config, done){ ...} }");
             }
 
-            
 
-            ruqq.arr.remove(paths, function(x){
+
+            ruqq.arr.remove(paths, function(x) {
                 return x === nodeModulesPath || x === globalPath;
             });
         });
