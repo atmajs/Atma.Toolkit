adc52bc57705501722101d7d456208e1301b04d1
Alexander Kit <source.kit@gmail.com>
2013-04-29 15:56:36 +0200

    [-] dev test suite

--- test/include.routes.js
+++
@@ -1,5 +0,0 @@
-include.cfg({
-     "lib": "/.import/libjs/{name}/lib/{name}.js",
-     "framework": "/.import/libjs/framework/lib/{name}.js",
-     "compo": "/.import/libjs/compos/{name}/lib/{name}.js"
-});
--- test/script/main.js
+++
@@ -1,8 +0,0 @@
-include.js({
-	lib: 'compo',
-	framework: 'dom/zepto'
-}).ready(function(){
-	
-	new Compo('#layout').render().insert(document.body);
-	
-});
