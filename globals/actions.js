432c7b7966766a1bb139a9e8154118096f7c5111
Alex Kit <source.kit@gmail.com>
2020-01-02 22:28:36 +0100

    bump

--- globals/actions.js
+++ globals/actions.js
@@ -1,56 +1,58 @@
 
 module.exports = {
-	actions: defaultActions()
+    actions: defaultActions()
 };
 
 function defaultActions() {
-	var actions = [
-		'concat', 
-		'copy',   
-		
-		'custom',
-		'template',
-		
-		'config',
-		'import',
-		
-		'jshint',
-		'uglify',
-		'watch',
-		
-		'release',
-		'bump',
-		'npm',
-		'plugin',
-		
-		'reference',
-		'server',
-		'shell',
-		
-		'transpile',
-		
-		// build includejs project
-		'build',
-		'project-import',
-		'project-reference',
-		
-		'atma-clone'
-	];
-	
-	var paths = actions.reduce((obj, action) => {
-		obj[action] = `/src/action/${action}.js`;
-		return obj;
-	}, {});
-	
-	// overrides
-	var solution = '/src/action/solution.js';
-	paths['project-import'] =
-		paths['project-reference'] =
-			paths['build'] = solution;
-	
-	
-	// aliases
+    var actions = [
+        'concat',
+        'copy',
+
+        'custom',
+        'template',
+
+        'config',
+        'import',
+
+        'jshint',
+        'uglify',
+        'watch',
+
+        'release',
+        'bump',
+        'npm',
+        'plugin',
+
+        'reference',
+        'server',
+        'shell',
+
+        'transpile',
+
+        // build includejs project
+        'build',
+        'project-import',
+        'project-reference',
+
+        'atma-clone',
+        'publish'
+    ];
+
+    var paths = actions.reduce((obj, action) => {
+        obj[action] = `/src/action/${action}.js`;
+        return obj;
+    }, {});
+
+    // overrides
+    var solution = '/src/action/solution.js';
+    paths['project-import'] =
+        paths['project-reference'] =
+        paths['build'] = solution;
+
+
+    // aliases
     paths['gen'] = paths['template'];
     paths['run'] = paths['custom'];
-	return paths;
+    paths['pub'] = paths['publish'];
+    return paths;
 }
